49563252d8021f22f8bb33b427b96c58b2c86924
sanjana <sanjanarm25@gmail.com>
2023-01-03 21:11:54 +0530

    palindrome and prime number problem

---
+++ palindrome.js
@@ -0,0 +1,15 @@
+let str="Sanjana";
+let temp=str;
+a="";
+for(let i=str.length-1;i>=0;i++)
+{
+ a=a+(str[i]);
+}
+  if(temp==a)
+  {
+      console.log("Yes");
+  }
+  else
+  {
+      console.log("No");
+  }
---
+++ primeornot.js
@@ -0,0 +1,16 @@
+let num=13;
+let a;
+for(a=2;a<num;a++)
+  {
+    if(num%a!=0)
+    {
+       break;
+    }
+  }
+   if(num==a)
+   {
+     console.log("Yes");
+   }
+else{
+  console.log("No");
+}
